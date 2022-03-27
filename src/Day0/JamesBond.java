5eb013eb83d95c0f06acc4248c6b5862e56265b4
SamilG <98919063+SamilG@users.noreply.github.com>
2022-03-27 09:26:44 -0400

    Create JamesBond.java

---
+++ src/Day0/JamesBond.java
@@ -0,0 +1,8 @@
+package day0;
+
+public class JamesBond{
+  public static main void (String [] args){
+  System.out.println("James Bond hacked the project);
+                     
+   }
+ }                
